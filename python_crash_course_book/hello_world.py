149ecb5d3652e2c3bee70082df74cf16cf8c1f26
gnozze <ezzong@gmail.com>
2025-06-29 18:10:20 +0700

    Chapter 1

--- python_crash_course_book/hello_world.py
+++ python_crash_course_book/hello_world.py
@@ -1,2 +1,12 @@
-# Printing "Hello World!"
-print("Hello World")
+# Using function directly
+print("Hello World!")
+
+# Using a variable
+# 'message' is a variable
+# The value is "Hello World!" as a string
+message = ("This is a code I wrote.")
+print(message)
+
+# Trying to create an error and trackback
+message = ("I hope it works as intended.")
+print(message)
