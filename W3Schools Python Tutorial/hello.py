46c51a08945d03c7e88ccc90b908b5ffcc777922
gnozze <ezzong@gmail.com>
2025-06-29 17:12:52 +0700

    Update hello.py

--- W3Schools Python Tutorial/hello.py
+++ W3Schools Python Tutorial/hello.py
@@ -1,1 +1,11 @@
-print ("Hello World!")
+#I want to make a simple identity program#
+first_name=input("What is your name? ")
+last_name=input
+gender=input("Are you a man (m) or a woman (w)? ")
+last_name = input("What is your last name? ")
+if gender == "m":
+    print(f"Hello Mr. {first_name} {last_name}!")
+elif gender == "w":
+    print(f"Hello Ms. {first_name} {last_name}!")
+else:
+    print(f"Hello {first_name} {last_name}!")
